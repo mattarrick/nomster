cdbad5cf22b7c27310b4b52b89dea054c08b9d76
Matt Arrick <mattarrick@gmail.com>
2019-04-23 23:53:47 +0000

    More validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :address, presence: true
+  validates :description, presence: true
 end
