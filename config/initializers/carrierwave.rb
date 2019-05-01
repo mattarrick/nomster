ceadbd38e3e2799a97e13d11a54f0e6e28df0ccb
Matt Arrick <mattarrick@gmail.com>
2019-05-01 23:06:47 +0000

    Pictures3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,10 +4,9 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAVQMYZ4HTP67BG5VQ',        # required
-    aws_secret_access_key: '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'        # required
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = "matt-arrick-nomster-bucket"              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]              # required
 end
 
-# puts ENV.keys
