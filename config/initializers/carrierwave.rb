93a69e56a95e462704c61e194e11d3c51573f8a5
Matt Arrick <mattarrick@gmail.com>
2019-05-02 04:49:07 +0000

    Edit carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,5 +8,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_public = false
 end
 
