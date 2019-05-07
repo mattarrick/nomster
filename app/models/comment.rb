66b55a6b759f107581b1dcbd2262a64aff87504d
Matt Arrick <mattarrick@gmail.com>
2019-05-07 18:41:30 +0000

    Send email on comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
 
   RATINGS = {
     'one star': '1_star',
@@ -13,4 +14,9 @@ class Comment < ApplicationRecord
   def humanized_rating
     RATINGS.invert[self.rating]
   end
+
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver_now
+  end
+
 end
