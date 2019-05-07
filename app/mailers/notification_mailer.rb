723c6b8765aa8d958cc1fd04f8c2267ace387870
Matt Arrick <mattarrick@gmail.com>
2019-05-07 18:03:42 +0000

    Previewing the Email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,7 +3,7 @@ class NotificationMailer < ApplicationMailer
 
   def comment_added
     mail(to: "mattarrick@gmail.com",
-      subject: "A comment has been added to your place")
+         subject: "A comment has been added to your place")
   end
 
 end
