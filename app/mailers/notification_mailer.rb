d451728eb22c4f1dccddd6dbbb09ecea0004f016
Matt Arrick <mattarrick@gmail.com>
2019-05-07 17:34:19 +0000

    Emailing basics

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,9 @@
+class NotificationMailer < ApplicationMailer
+  default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+    mail(to: "mattarrick@gmail.com",
+      subject: "A comment has been added to your place")
+  end
+
+end
