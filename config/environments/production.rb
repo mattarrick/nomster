04f7cb6caab5e80a31268cb1405cbb482a9e7b20
Matt Arrick <mattarrick@gmail.com>
2019-05-10 16:48:55 +0000

    Mailing on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,15 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://nomster-matt-arrick.herokuapp.com'}
   # Settings specified here will take precedence over those in config/application.rb.
-
+  ActionMailer::Base.smtp_settings = {
+      address: 'smtp.sendgrid.net',
+      port: '587',
+      authentication: :plain,
+      user_name: ENV['SENDGRID_USERNAME'],
+      password: ENV['SENDGRID_PASSWORD'],
+      domain: 'heroku.com',
+      enable_starttls_auto: true
+    }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
